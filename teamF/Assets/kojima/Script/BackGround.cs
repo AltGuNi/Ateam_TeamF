1fd60b6b005f72de7a3e9e0c05e607a1a1f379dd
Soma Kojima <kojisou1016@gmail.com>
2018-09-11 14:44:49 +0900

    背景を作成

---
+++ teamF/Assets/kojima/Script/BackGround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+    [SerializeField]
+    GameObject backGroundPrefab;
+    [SerializeField]
+    float speed;
+    
+    float startLine = 53.0f;
+    float instanceLine = -43.0f;
+    float endLine = -53.0f;
+
+    bool instanceFlag = false;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, -speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!instanceFlag &&
+            gameObject.transform.position.y < instanceLine)
+        {
+            instanceFlag = true;
+            GameObject obj = Instantiate(backGroundPrefab);
+            obj.gameObject.transform.position = new Vector2(0.0f, startLine);
+        }
+		if(gameObject.transform.position.y < endLine)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
