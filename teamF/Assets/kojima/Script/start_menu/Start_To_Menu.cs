8f90e0e6a3e75718a95805e9423c3645842f1da3
AltGuNi <kobayashi.kodai@x.colopl.co.jp>
2018-09-15 19:24:44 +0900

    スタート画面->メインメニュー->画面のタッチによるシーン遷移を実装

---
+++ teamF/Assets/kojima/Script/start_menu/Start_To_Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TouchScript.Gestures;
+using UnityEngine.SceneManagement;
+
+public class Start_To_Menu : MonoBehaviour {
+    public string move_scene = "main_menu";
+
+    private void OnEnable()
+    {
+        GetComponent<PressGesture>().Pressed += OnPressed;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<PressGesture>().Pressed -= OnPressed;
+    }
+
+    private void OnPressed(object sender, EventArgs e)
+    {
+        SceneManager.LoadScene(move_scene);
+    }
+
+}
