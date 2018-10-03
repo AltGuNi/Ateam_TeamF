6238dd826a5c4441d947d97e87a7a4a7fa635c62
AltGuNi <kobayashi.kodai@x.colopl.co.jp>
2018-10-03 13:57:21 +0900

    スタート画面のボタン調整

---
+++ teamF/Assets/kojima/Script/start_menu/flash_Button.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class flash_Button : MonoBehaviour {
+    float a_color;
+    bool flag_G;
+	public GameObject dark;
+	float dark_dist = 0;
+    // Use this for initialization
+    void Start () {
+        a_color = 0;
+    }
+    // Update is called once per frame
+    void Update () {
+            //テキストの透明度を変更する
+			if(a_color > 0){
+				dark.GetComponent<SpriteRenderer>().color  += new Color(0.0f, 0.0f, 0.0f, dark_dist);
+			}
+
+			if (flag_G) {
+                a_color -= Time.deltaTime;
+				dark_dist = Time.deltaTime;
+			}else{
+                a_color += Time.deltaTime;
+				dark_dist = -Time.deltaTime;
+			}
+
+            if (a_color < -0.8) {
+                a_color = 0;
+                flag_G = false;
+            }else if(a_color > 1){
+                a_color = 1;
+                flag_G = true;
+            }
+    }
+}
